acb724e4053fbf3ab08549722cb30b0ed466a2cf
kaysee clintone <kayseeclintone@gmail.com>
2025-07-11 16:31:02 +0300

    Create index.js

---
+++ app/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { extractCookies } = require('./puppeteer');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<a href="/extract">Click to extract cookies</a>');
+});
+
+app.get('/extract', async (req, res) => {
+  try {
+    await extractCookies();
+    res.download('./cookies.txt');
+  } catch (err) {
+    res.status(500).send("Failed to extract cookies: " + err.message);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Running on http://localhost:${PORT}`);
+});
