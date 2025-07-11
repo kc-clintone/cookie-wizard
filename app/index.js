4187e2855a656c8b93cff045a8a5c03b712ed4fa
kaysee clintone <kayseeclintone@gmail.com>
2025-07-11 18:01:43 +0000

    fixing errors

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { extractCookies } = require('./puppeteer');
+const { extractCookies } = require('./pupp');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
