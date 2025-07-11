2632b99a585f8320978202e8e550a56bca6668c1
kc-clintone <112693204+clintoo@users.noreply.github.com>
2025-07-11 21:32:47 +0300

    Update pupp.js

--- app/pupp.js
+++ app/pupp.js
@@ -2,7 +2,10 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 async function extractCookies() {
-  const browser = await puppeteer.launch({ headless: false }); // login manually
+  const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+
   const page = await browser.newPage();
   await page.goto('https://www.youtube.com');
 
